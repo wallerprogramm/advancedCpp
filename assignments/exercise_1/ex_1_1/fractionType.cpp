34eeb4580d96faf2e1a0df528435e7441866401d
wallerprogramm <dominik.sommer@uzh.ch>
2018-10-03 17:29:34 +0200

    assignment 1.1

--- assignments/exercise_1/ex_1_1/fractionType.cpp
+++ assignments/exercise_1/ex_1_1/fractionType.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int gcf(int a, int b) {
-  return b == 0 ? a : gcf(b, a % b)
+  return b == 0 ? a : gcf(b, a % b);
 }
 
 int lcm(int a, int b) {
@@ -34,7 +34,7 @@ public:
     fraction operator-(fraction b) {
       int d = lcm(denom, b.get_denominator());
       int c = (cntr*d/denom) - (b.get_counter()*d/b.get_denominator());
-      return fraction(c, d)
+      return fraction(c, d);
     }
     fraction operator*(fraction b) {
       int f1 = gcf(cntr, b.denom); //b.cntr ---> we can access privates of same type
@@ -45,7 +45,7 @@ public:
       fraction invers_b = fraction(b.get_denominator(), b.get_counter());
       return (*this) * invers_b;
     }
-    inline void check_char(iostream &is, char ch) {
+    inline static void check_char(istream &is, char ch) {
       char c;
       is >> c;
       if (c != ch) {
@@ -53,20 +53,69 @@ public:
         is.setstate(ios::badbit);
       }
     }
-    ostream &operator<<(ostream &os, fraction f) {
+    friend ostream &operator<<(ostream &os, fraction f) {
       os << '(' << f.cntr << '/' << f.denom << ')';
       return os;
     }
-    istream &operator>>(istream &is, fraction &f) {
+    friend istream &operator>>(istream &is, fraction &f) {
       fraction g;
-      check_char('('); is >> g.cntr;
-      check_char('/'); is >> g.denom;
-      check_char(')'); if(is) f=g;
+      check_char(is, '('); is >> g.cntr;
+      check_char(is, '/'); is >> g.denom;
+      check_char(is, ')'); if(is) f=g;
       return is;
     }
+};
+
+void static_example() {
+  fraction f1 = fraction(3, 5);
+  fraction f2 = fraction(15, 7);
+  cout << "Hi user, this driver tests the operators +, -, *, / with the following fractions:" << endl;
+  cout << "fraction f1: " << f1 << endl;
+  cout << "fraction f2: " << f2 << endl;
+
+  cout << f1 << " + " << f2 << " = " << f1+f2 << endl;
+  cout << f1 << " - " << f2 << " = " << f1-f2 << endl;
+  cout << f1 << " * " << f2 << " = " << f1*f2 << endl;
+  cout << f1 << " / " << f2 << " = " << f1/f2 << endl;
 }
 
-void main(int argc, char *argv[]) {
-  fraction f(atoi(argv[1]), atoi(argv[2]));
-  cout << f.get_counter() << ":" << f.get_denominator() << endl '\n';
+void interactive_example() {
+  fraction f1;
+  fraction f2;
+  char operation;
+  cout << "Enter your first fraction in the following way: (counter/denominator): ";
+  cin >> f1;
+  cout << endl;
+  cout << "Enter your favourite operator form +, -, *, /: ";
+  cin >> operation;
+  cout << endl;
+  cout << "Enter your second fraction in the following way: (counter/denominator): ";
+  cin >> f2;
+  cout << endl;
+
+  switch (operation) {
+    case '+': cout << f1 << " + " << f2 << " = " << f1+f2 << endl;
+    break;
+    case '-': cout << f1 << " - " << f2 << " = " << f1-f2 << endl;
+    break;
+    case '*': cout << f1 << " * " << f2 << " = " << f1*f2 << endl;
+    break;
+    case '/': cout << f1 << " / " << f2 << " = " << f1/f2 << endl;
+    break;
+    default: cout << "Unluckily, " << operation << "isn't an operator!" << endl;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  char example_style;
+  cout << "Dear user, choose either a static example by entering s or a interactive example by entering i: ";
+  cin >> example_style;
+  cout << endl;
+  switch (example_style) {
+    case 's': static_example();
+    break;
+    case 'i': interactive_example();
+    break;
+    default: cout << "Please read the instructions carfully!" << endl;
+  }
 }
