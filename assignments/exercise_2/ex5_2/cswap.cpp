2f0f420638db0bc8afea82327d91680a24ec8cae
wallerprogramm <dominik.sommer@uzh.ch>
2018-10-14 22:47:58 +0200

    Separate swap and cswap.

--- assignments/exercise_2/ex5_2/cswap.cpp
+++ assignments/exercise_2/ex5_2/cswap.cpp
@@ -2,35 +2,34 @@
 
 using namespace std;
 
-void swap(int &a, int &b) {
-  int c = a;
-  a = b;
-  b = c;
-}
-
-// void c_swap(int *x, int *y) {
-//   int z = *x;
-//   *x = *y;
-//   *y = z;
+// void swap(int &a, int &b) {
+//   int c = a;
+//   a = b;
+//   b = c;
 // }
 
+void c_swap(int *x, int *y) {
+  int z = *x;
+  *x = *y;
+  *y = z;
+}
+
 int main(int argc, char const *argv[]) {
-  int a = 3;
-  int b = 5;
-  // int x = 3;
-  // int y = 5;
+  // int a = 3;
+  // int b = 5;
+  int x = 3;
+  int y = 5;
 
   // cout << "a: " << a << endl;
   // cout << "b: " << b << endl;
   // cout << "x: " << x << endl;
   // cout << "y: " << y << endl;
   // cout << "-------------------------";
-  swap(a, b);
-  // c_swap(&x, &y);
+  // swap(a, b);
+  c_swap(&x, &y);
   // cout << "a: " << a << endl;
   // cout << "b: " << b << endl;
   // cout << "x: " << x << endl;
   // cout << "y: " << y << endl;
-  /* code */
   return 0;
 }
